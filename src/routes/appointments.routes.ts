0b0659ef232a3ce87aa773db10f11fad4b97c4ea
Carlos Kenny <carlos.kenny.ca@gmail.com>
2020-09-04 00:12:11 -0300

    create repositories and models (entidade)

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,23 +1,26 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
 import { startOfHour, parseISO } from 'date-fns';
+import AppointmentRepository from '../repositories/Appointment.Repository';
 
 const appointmentsRouter = Router();
-
-const appointments = [];
+const appointmentsRepository = new AppointmentRepository();
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
   const dateParsed = startOfHour(parseISO(date));
 
-  const appointment = {
-    id: uuid(),
-    provider,
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
     dateParsed,
-  };
+  );
 
-  appointments.push(appointment);
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, dateParsed);
 
   return response.json(appointment);
 });
